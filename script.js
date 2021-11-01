4c74fcb56927666f7b7196d716dd05226e0bd2f3
Tara Pedigo <tkirkhus@gmail.com>
2021-11-01 12:57:23 -0400

    Create functions for temporary fill color & clicked fill color

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ for (i=0; i<(16*16); i++) {
     square.classList.add("square");
     container.appendChild(square);
 }
+
+function hover(e) {
+    e.target.classList.add("hover");
+}
+
+function normal(e) {
+    e.target.classList.remove("hover");
+}
+
+function fill(e) {
+    if (e.buttons == 0) {
+        return;
+    } else {
+        e.target.classList.add("filled");
+    }
+}
+
+document.querySelectorAll(".square").forEach((square) => {
+    square.addEventListener("mouseover", hover);
+    square.addEventListener("mouseout", normal);
+    square.addEventListener("mousemove", fill);
+});
